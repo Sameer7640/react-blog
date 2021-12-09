0114e6170c80811695af13d6cba5006fd2c8de98
Sameer <sameerhussain7640@gmail.com>
2021-12-09 18:41:34 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return(
+    <div className="header">
+      Hello World
+    </div>
+  )
+}
+export const Application = () => {
+  return(
+    <div className="header">
+      Bye World
+    </div>
+  )
+}
+
+export default App;
